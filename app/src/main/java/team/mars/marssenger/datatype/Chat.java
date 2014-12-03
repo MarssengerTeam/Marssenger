6e0c72ca37beef24627efde2acb3d28b8645737e
kernproblem <kernproblem@live.de>
2014-12-03 15:27:14 +0100

    added datatypes Chat + Message

--- app/src/main/java/team/mars/marssenger/datatype/Chat.java
+++ app/src/main/java/team/mars/marssenger/datatype/Chat.java
@@ -1,7 +1,39 @@
 package team.mars.marssenger.datatype;
 
-/**
- * Created by root on 03.12.14.
- */
+
 public class Chat {
+    private long id;
+    private String name;
+    private String number;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return name;
+    }
 }
--- app/src/main/java/team/mars/marssenger/datatype/Message.java
+++ app/src/main/java/team/mars/marssenger/datatype/Message.java
@@ -1,7 +1,53 @@
 package team.mars.marssenger.datatype;
 
+
 /**
- * Created by root on 03.12.14.
+ * Created by TimoBlock on 24.07.2014.
  */
+
 public class Message {
+    private long id;
+    private String message;
+    private String reciver;
+    private String timestamp;
+    private String sender;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getReciver() {return reciver;}
+
+    public String getTimestamp(){return timestamp;}
+
+    public String getSender(){return  sender;}
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return message;
+    }
+
+    public void setSender(String sender){this.sender = sender;}
+
+    public void setReciver(String reciver) {
+        this.reciver = reciver;
+    }
+
+    public void setTime(String time) {
+        this.timestamp = time;
+    }
 }
