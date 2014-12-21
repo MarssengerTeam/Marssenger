9ffffd8bf241fc814055d071c2d854509e97ddab
Nicolas <nicolastorres55@gmail.com>
2014-12-21 16:25:00 +0100

    more tests chats

--- app/src/main/java/team/mars/marssenger/main/MainInteractorImpl.java
+++ app/src/main/java/team/mars/marssenger/main/MainInteractorImpl.java
@@ -76,14 +76,30 @@ public class MainInteractorImpl implements MainInteractor {
 
     private void createTestChats(){
         int sender =(int) (Math.random()*2);
-        if(chatDatabase.getAllChat().size()>3){
-
-        }else{
+        if(chatDatabase.getAllChat().size()<8) {
             chatDatabase.createChat("Timo", "0157700000");
             messageDatabase.createMessage("1st Message", sender, 0, 0);
-            chatDatabase.createChat("Jan Niklas", "0157712345");
-            messageDatabase.createMessage("1st Message",sender,1,0);
 
+            chatDatabase.createChat("Jan Niklas", "0157712345");
+            messageDatabase.createMessage("1st Message", sender, 1, 0);
+
+            chatDatabase.createChat("Noli", "015712563485");
+            messageDatabase.createMessage("last message this is really interesting oh my god!", sender, 2, 0);
+
+            chatDatabase.createChat("Nils", "015748569521");
+            messageDatabase.createMessage("this is also a really long message to see if it breaks the layout", sender, 3, 0);
+
+            chatDatabase.createChat("Nicolas", "015784594253");
+            messageDatabase.createMessage("to have a beard is to have democracy", sender, 4, 0);
+
+            chatDatabase.createChat("Mister Null","000000000000");
+            messageDatabase.createMessage("...",sender,5,0);
+
+            chatDatabase.createChat("Miss One","111111111111");
+            messageDatabase.createMessage("oneoneoneoneoneoneone",sender,5,0);
+
+            chatDatabase.createChat("Null Jr.","015697536458");
+            messageDatabase.createMessage("nullnull null null null",sender,6,0);
         }
     }
 
