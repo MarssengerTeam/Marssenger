d93785f07f5f4630723edc5461eb25382aa21325
keinproblem <noli.m@qq.com>
2014-12-27 19:35:13 +0100

    first Https posting-framework

---
+++ app/src/main/java/team/mars/marssenger/communication/HttpsBackground.java
@@ -0,0 +1,7 @@
+package team.mars.marssenger.communication;
+
+/**
+ * Created by Noli on 25.12.2014.
+ */
+public class HttpsBackground {
+}
---
+++ app/src/main/java/team/mars/marssenger/communication/HttpsBackgroundService.java
@@ -0,0 +1,8 @@
+package team.mars.marssenger.communication;
+
+/**
+ * Created by Noli on 25.12.2014.
+ */
+public class HttpsBackgroundService {
+
+}
