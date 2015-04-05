953aac49b9dcd07677092468b56a0143501241ce
Vierheller <j.vierheller@outlook.de>
2015-04-05 16:34:50 +0200

    enhanced ContactListingActivity

--- app/src/main/java/team/mars/marssenger/main/ContactListingActivity.java
+++ app/src/main/java/team/mars/marssenger/main/ContactListingActivity.java
@@ -1,19 +1,42 @@
 package team.mars.marssenger.main;
 
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.Contacts;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListAdapter;
+import android.widget.SimpleCursorAdapter;
 
 import team.mars.marssenger.R;
 
-public class ContactListingActivity extends ActionBarActivity {
+public class ContactListingActivity extends ListActivity {
+    private final int PICK_CONTACT = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_listing);
+
+        Cursor mCursor = this.getContentResolver().query(Contacts.People.CONTENT_URI, null, null, null, null);
+        startManagingCursor(mCursor);
+
+        ListAdapter adapter = new SimpleCursorAdapter(this, R.layout.contacts_item, mCursor, new String[]{Contacts.People.NAME, Contacts.People.NUMBER},  new int[] {R.id.text1, R.id.text2});
+        setListAdapter(adapter);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
 
 
     @Override
@@ -22,6 +45,7 @@ public class ContactListingActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_contact_listing, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
